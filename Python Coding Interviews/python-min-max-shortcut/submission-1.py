91502618391c9918dd5a4d926c47bcae2d1bba49
kaili shoop <154503006+kshoop28@users.noreply.github.com>
2026-07-11 01:56:44 -0400

    Add: python-min-max-shortcut - submission-1

---
+++ Python Coding Interviews/python-min-max-shortcut/submission-1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def disallow_negatives(num: int) -> int:
+    return max(0, num)
+
+
+def max_difference(nums: List[int]) -> int:
+    if not nums or len(nums) < 2:
+        raise ValueError("List must contain at least two elements to calculate a difference.")
+    
+    return max(nums[i] - nums[i - 1] for i in range(1, len(nums)))
+# do not modify below this line
+print(disallow_negatives(-2))
+print(disallow_negatives(-1))
+print(disallow_negatives(0))
+print(disallow_negatives(1))
+print(disallow_negatives(2))
+
+print(max_difference([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(max_difference([1, 2, 3, 4, 5, 6, 8, 9]))
+print(max_difference([10, 1, 3, 7]))
+print(max_difference([2, 4, 7, 5, 7, 8, 4, 2]))
